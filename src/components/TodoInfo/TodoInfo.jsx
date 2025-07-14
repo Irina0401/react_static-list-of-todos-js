199ee674713a0d2e027842b93fdd562cf96e3b98
Irina <weedfox053@gmail.com>
2025-07-14 23:40:12 +0300

    static list of TODOs

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,2 +1,9 @@
+import { UserInfo } from "../UserInfo/UserInfo";
+
 // Add the required props
-export const TodoInfo = () => <>TodoInfo markup</>;
+export const TodoInfo = ({info, getUser}) => <>
+<article className="TodoInfo TodoInfo--completed">
+<h2 className="TodoInfo__title">{info.title}</h2>
+<UserInfo user = {getUser}/>
+</article>
+</>;
--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,2 +1,10 @@
+import { TodoInfo } from "../TodoInfo/TodoInfo";
+
 // Add the required props
-export const TodoList = () => <>TodoList markup</>;
+export const TodoList = ({list, getUsers}) => <>
+<section className="TodoList">
+  {list.map (l => (
+<TodoInfo info = {l} key = {l.id} getUsers = {getUsers}/>
+  ))}
+    </section>
+</>;
